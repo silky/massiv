50ad4d0780dfbefca3b1228c39809bed9ef7a7a4
Alexey Kuleshevich <alexey@fpcomplete.com>
2018-02-20 17:34:37 +0300

    Deadlock reproduced with Repa+Accelerate

--- massiv-bench/bench/Accelerate.hs
+++ massiv-bench/bench/Accelerate.hs
@@ -1,64 +1,42 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ViewPatterns     #-}
 module Main where
-
-import           Bench.Common                      (lightFunc)
-import           Bench.Massiv.Array                as M
-import           Bench.Repa                as R
-import           Data.Array.Repa       as R
 
 import           Criterion.Main
 import           Data.Array.Accelerate             as A
 import           Data.Array.Accelerate.LLVM.Native as A
---import           Data.Array.Accelerate.Interpreter as A
-import Data.Massiv.Core.Scheduler
+import           Data.Array.Repa                   as R
 import           Data.Functor.Identity
+import           Prelude                           as P
 
 accArrLightDIM2 :: (Int, Int) -> A.Array A.DIM2 Double
 accArrLightDIM2 (m, n) = A.fromFunction (A.Z A.:. m A.:. n) (\ (A.Z A.:. i A.:. j) -> lightFunc i j)
-
-
-accArrLightDIM2' :: (Int, Int) -> Acc (A.Array A.DIM2 Double)
-accArrLightDIM2' (m, n) =
-  generate (lift (A.Z A.:. m A.:. n)) (\(unlift -> A.Z A.:. i A.:. j) -> lightFuncExp i j)
-
 
 lightFuncExp :: Exp Int -> Exp Int -> Exp Double
 lightFuncExp i j =
   A.sin (A.fromIntegral (i A.^ (2 :: Exp Int) + j A.^ (2 :: Exp Int)) :: Exp Double)
 
-sumAccArr :: (A.Elt e, A.Shape t, Prelude.Num (Exp e)) => A.Array t e -> e
+sumAccArr :: (A.Elt e, A.Shape t, P.Num (Exp e)) => A.Array t e -> e
 sumAccArr = (`indexArray` A.Z) . run . A.sum . A.flatten . use
 
+lightFunc :: Int -> Int -> Double
+lightFunc i j = sin (P.fromIntegral (i P.^ (2 :: Int) + j P.^ (2 :: Int)) :: Double)
 
-sumAccArr' :: (A.Elt e, A.Shape t, Prelude.Num (Exp e)) => Acc (A.Array t e) -> e
-sumAccArr' = (`indexArray` A.Z) . run . A.sum . A.flatten
+tupleToSh2 :: (Int, Int) -> R.DIM2
+tupleToSh2 (i, j) = R.Z R.:. i R.:. j
 
+arrDLightSh2 :: R.DIM2 -> R.Array D R.DIM2 Double
+arrDLightSh2 sz = R.fromFunction sz (\(R.Z R.:. i R.:. j) -> lightFunc i j)
 
 main :: IO ()
 main = do
   let t2 = (1600, 1200) :: (Int, Int)
+      repaArr = computeUnboxedS (arrDLightSh2 (tupleToSh2 t2))
+      accArr = accArrLightDIM2 t2
   defaultMain
     [ bgroup
         "Sum"
-        [ env
-                (return (computeUnboxedS (arrDLightSh2 (tupleToSh2 t2))))
-                (bench "Repa DIM2 U" . whnf (runIdentity . R.sumAllP))
-        -- , env
-        --     (return (computeAs U (arrDLightIx2 Par (tupleToIx2 t2))))
-        --     (bench "Array U Ix2" . whnf M.sum)
-        , env (return (accArrLightDIM2 t2)) (bench "Accelerate DIM2" . whnf sumAccArr)
-        , env
-          (return (computeUnboxedS (arrDLightSh2 (tupleToSh2 t2))))
-          (bench "Repa DIM2 U" . whnf (runIdentity . R.sumAllP))
-        -- , env (return t2) (bench "Array U Ix2" . whnf (M.sum . arrDLightIx2 Par . tupleToIx2))
+        [ bench "Repa DIM2 U" $ whnf (runIdentity . R.sumAllP) repaArr
+        , bench "Accelerate DIM2" $ whnf sumAccArr accArr
+        , bench "Repa DIM2 U" $ whnf (runIdentity . R.sumAllP) repaArr
         ]
-    -- , bgroup
-    --     "Create/Sum"
-    --     [ env (return t2) (bench "Array U Ix2" . whnf (M.sum . arrDLightIx2 Par . tupleToIx2)) ]
-    -- , bgroup
-    --     "Create/Sum Acc" [
-    --      env (return t2) (bench "Accelerate DIM2 generate" . whnf (sumAccArr' . accArrLightDIM2'))
-    --     ]
     ]
-  globalWorkersMVar `seq` print "Make sure global scheduler isn't being GCed"
